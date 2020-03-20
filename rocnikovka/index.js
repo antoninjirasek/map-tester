f9e654dc2b0a63ec6496cefeb30a279d5c9d4b31
antoninjirasek <44426074+antoninjirasek@users.noreply.github.com>
2020-03-20 14:07:34 +0100

    Add files via upload

---
+++ rocnikovka/index.js
@@ -0,0 +1,8 @@
+function mapVisible() {
+    document.getElementById("map").style.visibility = "";
+    document.getElementById('map').style.right="25%";;
+    document.getElementById('map').style.top="35";
+};
+function mapInvisible() {
+    document.getElementById("map").style.visibility = "hidden";
+};
